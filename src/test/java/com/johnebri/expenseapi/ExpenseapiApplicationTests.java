3fd493bf065fb58f32d5a67100b2e07b569275c1
johnebri <john.ebri@yahoo.com>
2020-06-23 19:11:23 +0100

    chore(app):set up

---
+++ src/test/java/com/johnebri/expenseapi/ExpenseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnebri.expenseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
